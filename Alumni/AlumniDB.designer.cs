623a2d2ccc25c3eeae7bbb39f8e4834e4a3f9933
Shindo <cn.shindo@gmail.com>
2015-08-05 12:38:18 +0800

    Donations update.

--- Alumni/AlumniDB.designer.cs
+++ Alumni/AlumniDB.designer.cs
@@ -49,6 +49,9 @@ namespace Alumni
     partial void InsertUsers(AlumniDB.Users instance);
     partial void UpdateUsers(AlumniDB.Users instance);
     partial void DeleteUsers(AlumniDB.Users instance);
+    partial void InsertDonations(AlumniDB.Donations instance);
+    partial void UpdateDonations(AlumniDB.Donations instance);
+    partial void DeleteDonations(AlumniDB.Donations instance);
     #endregion
 		
 		public DBDataContext() : 
@@ -113,14 +116,6 @@ namespace Alumni
 			}
 		}
 		
-		public System.Data.Linq.Table<AlumniDB.Donations> Donations
-		{
-			get
-			{
-				return this.GetTable<AlumniDB.Donations>();
-			}
-		}
-		
 		public System.Data.Linq.Table<AlumniDB.Links> Links
 		{
 			get
@@ -150,6 +145,14 @@ namespace Alumni
 			get
 			{
 				return this.GetTable<AlumniDB.Users>();
+			}
+		}
+		
+		public System.Data.Linq.Table<AlumniDB.Donations> Donations
+		{
+			get
+			{
+				return this.GetTable<AlumniDB.Donations>();
 			}
 		}
 	}
@@ -1120,87 +1123,6 @@ namespace AlumniDB
 		}
 	}
 	
-	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Donations")]
-	public partial class Donations
-	{
-		
-		private int _DonationId;
-		
-		private string _Name;
-		
-		private string _Amount;
-		
-		private System.DateTime _Date;
-		
-		public Donations()
-		{
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_DonationId", AutoSync=AutoSync.Always, DbType="Int NOT NULL IDENTITY", IsDbGenerated=true)]
-		public int DonationId
-		{
-			get
-			{
-				return this._DonationId;
-			}
-			set
-			{
-				if ((this._DonationId != value))
-				{
-					this._DonationId = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(MAX) NOT NULL", CanBeNull=false)]
-		public string Name
-		{
-			get
-			{
-				return this._Name;
-			}
-			set
-			{
-				if ((this._Name != value))
-				{
-					this._Name = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Amount", DbType="NVarChar(MAX) NOT NULL", CanBeNull=false)]
-		public string Amount
-		{
-			get
-			{
-				return this._Amount;
-			}
-			set
-			{
-				if ((this._Amount != value))
-				{
-					this._Amount = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Date", DbType="DateTime NOT NULL")]
-		public System.DateTime Date
-		{
-			get
-			{
-				return this._Date;
-			}
-			set
-			{
-				if ((this._Date != value))
-				{
-					this._Date = value;
-				}
-			}
-		}
-	}
-	
 	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Links")]
 	public partial class Links : INotifyPropertyChanging, INotifyPropertyChanged
 	{
@@ -1766,6 +1688,140 @@ namespace AlumniDB
 		{
 			this.SendPropertyChanging();
 			entity.Users = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Donations")]
+	public partial class Donations : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _DonationId;
+		
+		private string _Name;
+		
+		private string _Amount;
+		
+		private System.DateTime _Date;
+		
+    #region 可扩展性方法定义
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnDonationIdChanging(int value);
+    partial void OnDonationIdChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    partial void OnAmountChanging(string value);
+    partial void OnAmountChanged();
+    partial void OnDateChanging(System.DateTime value);
+    partial void OnDateChanged();
+    #endregion
+		
+		public Donations()
+		{
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_DonationId", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int DonationId
+		{
+			get
+			{
+				return this._DonationId;
+			}
+			set
+			{
+				if ((this._DonationId != value))
+				{
+					this.OnDonationIdChanging(value);
+					this.SendPropertyChanging();
+					this._DonationId = value;
+					this.SendPropertyChanged("DonationId");
+					this.OnDonationIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(MAX) NOT NULL", CanBeNull=false)]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Amount", DbType="NVarChar(MAX) NOT NULL", CanBeNull=false)]
+		public string Amount
+		{
+			get
+			{
+				return this._Amount;
+			}
+			set
+			{
+				if ((this._Amount != value))
+				{
+					this.OnAmountChanging(value);
+					this.SendPropertyChanging();
+					this._Amount = value;
+					this.SendPropertyChanged("Amount");
+					this.OnAmountChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Date", DbType="DateTime NOT NULL")]
+		public System.DateTime Date
+		{
+			get
+			{
+				return this._Date;
+			}
+			set
+			{
+				if ((this._Date != value))
+				{
+					this.OnDateChanging(value);
+					this.SendPropertyChanging();
+					this._Date = value;
+					this.SendPropertyChanged("Date");
+					this.OnDateChanged();
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
 		}
 	}
 }
